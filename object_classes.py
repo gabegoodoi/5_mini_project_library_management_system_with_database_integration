ba7c0ee1026206fd6ea4a6c0f44b046e7775f177
gabegodoi <gabegodoi@gmail.com>
2024-08-31 15:57:16 -0700

    updated object class

--- object_classes.py
+++ object_classes.py
@@ -9,14 +9,8 @@ class Book:
 class User:
     def __init__(self, name, lib_id, borrowed_books=None):
         self.name = name
-        self.__lib_id = lib_id
+        self.lib_id = lib_id
         self.borrowed_books = borrowed_books if borrowed_books is not None else []
-
-    def get_lib_id(self):
-        return self.__lib_id
-
-    def set_lib_id(self, new_id=None):
-        self.__lib_id = new_id
 
 class Author:
     def __init__(self, name, bio=None):
